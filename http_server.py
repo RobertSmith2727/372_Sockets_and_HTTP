53098e07d5d8442c9d1680b6a2366476ead4940b
robertsmith2727 <smithro8@oregonstate.edu>
2023-10-17 16:35:33 -0700

    1

--- http_server.py
+++ http_server.py
@@ -1,9 +1,13 @@
 # code derived from:
 # https://www.geeksforgeeks.org/python-binding-and-listening-with-sockets/
 # https://www.pubnub.com/blog/socket-programming-in-python-client-server-p2p/
+# code and comments derived from: Chapter 2.7 of course textbook
 
 import socket
+
+# creates client socket called plug
 plug = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# creates
 localHost = '127.0.0.1', 3000
 
 data = b"HTTP/1.1 200 OK\r\n"\
@@ -11,14 +15,21 @@ data = b"HTTP/1.1 200 OK\r\n"\
  b"<html>Congratulations! You've downloaded the first Wireshark lab file!</html>\r\n"
 
 getter = "GET /HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n"
+# opens the socket
 plug.bind(localHost)
+# lets server listen for connection request
 plug.listen()
-
+# the client initiates the accept and creates "plug" as the socket
 connection, address = plug.accept()
+# receives and saves the connection response
 response = connection.recv(10000)
+# prints the connection response
 print(response.decode())
+# sends the message saved as "data"
 connection.send(data)
+# prints the message
 print(data)
+# closes connection
 plug.close()
 
 
--- sockets_pt2.py
+++ sockets_pt2.py
@@ -2,16 +2,24 @@
 # https://docs.python.org/3/howto/sockets.html
 # https://www.internalpointers.com/post/making-http-requests-sockets-python
 # https://hackernoon.com/resolving-typeerror-a-bytes-like-object-is-required-not-str-in-python
+# code and comments derived from: Chapter 2.7 of course textbook
 
 import socket
+# creates client socket called plug
 plug = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = "gaia.cs.umass.edu", 80
-plug.connect(IP)
+
+# initiates client/server connection
+plug.connect(("gaia.cs.umass.edu", 80))
 get = b"GET /wireshark-labs/HTTP-wireshark-file3.html HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n"
+
+# sends client get request to TCP connection
 plug.sendall(get)
-
+# receives characters sent by server
 response = plug.recv(10000)
-
+# closes the socket
+plug.close()
+# prints server message
 print(response.decode())
+
 
 
